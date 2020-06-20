fbde62e0b232ea563009ecd30ca71fb3e2b33498
faridimanzade <faridfi@code.edu.az>
2020-06-20 20:22:15 +0400

    Bug Fixed

--- ATM_Ex/ATM_Ex/Program.cs
+++ ATM_Ex/ATM_Ex/Program.cs
@@ -18,18 +18,19 @@ namespace ATM_Ex
             users[2] = new User("threeName", "threeSurname", "threePan", "threePin", "threeCvc", "threeExpDate", 300);
 
 
-            User.PinAccepter(users);
+            Action action = new Action();
+            action.PinAccepter(users);
         }
     }
 
+
     class Card
     {
-        protected string PAN;
-        protected string PIN;
-        protected string CVC;
-        protected string Expiredate;
-        protected double Balance;
-
+        public string PAN { get; set; }
+        public string PIN { get; set; }
+        public string CVC { get; set; }
+        public string Expiredate { get; set; }
+        public double Balance { get; set; }
         public Card(string pAN, string pIN, string cVC, string expiredate, double balance)
         {
             this.PAN = pAN;
@@ -40,66 +41,61 @@ namespace ATM_Ex
         }
     }
 
-    class User : Card
-    {
-        private string Name;
-        private string Surname;
-        Card CreditCard;
 
-        // CONSTRUCTOR
-        public User(string name, string surname, string pAN, string pIN, string cVC, string expiredate, double balance) : base(pAN, pIN, cVC, expiredate, balance)
+    class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Card Creditcard;
+
+        public User(string name, string surname, string pAN, string pIN, string cVC, string expiredate, double balance)
         {
             this.Name = name;
             this.Surname = surname;
+            this.Creditcard = new Card(pAN, pIN, cVC, expiredate, balance);
         }
+    }
 
-        //  Static method gets the PIN as an input
-        public static void PinAccepter(User[] users)
+
+    class Action
+    {
+        //  Method which gets pin and checks whether there is user matching with given pin
+        public void PinAccepter(User[] users)
         {
             Console.WriteLine("Enter PIN:");
             string userPin = Console.ReadLine();
 
             for (int userId = 0; userId < users.Length; userId++)
             {
-                if (users[userId].PinChecker(userPin, users, userId))
+                if (users[userId].Creditcard.PIN == userPin)
                 {
+                    Console.WriteLine($"Welcome {users[userId].Name}  {users[userId].Surname}");
+                    OperationDecider(users[userId]);
                     break;
                 }
+                //else
+                //{
+                //    PinAccepter(users);
+                //}
             }
         }
 
-        //  Method which checks whether pin matcher with the any of the users 
-        private bool PinChecker(string userPin, User[] users, int userId)
-        {
-            if (PIN.Equals(userPin))
-            {
-                Console.WriteLine($"\n Welcome {Name} {Surname} !!!");
-                OperationDecider(users, userId);
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("\nIncorrect Pin");
-                PinAccepter(users);
-                return false;
-            }
-        }
 
         //  Method which defines to show the balance or get the cash
-        private void OperationDecider(User[] users, int userId)
+        private void OperationDecider(User user)
         {
             Console.WriteLine("\nEnter Choice of Operation ( '1' to see the balance || '2' to get money ) :");
             int choice = Int32.Parse(Console.ReadLine());
             if (choice == 1)
             {
-                Console.WriteLine($"Balance is -> {Balance}");
+                Console.WriteLine($"Balance is -> {user.Creditcard.Balance}");
             }
             else if (choice == 2)
             {
                 Console.WriteLine("\nEnter amount that you want to get");
                 Console.WriteLine("\n 1. 10 AZN\n 2. 20 AN\n 3. 50 AZN\n 4. 100 AZN\n 5. CustomAmount");
                 int cash = Int32.Parse(Console.ReadLine());
-                GetCash(cash, users, userId);
+                GetCash(cash, user);
             }
             else
             {
@@ -107,25 +103,27 @@ namespace ATM_Ex
             }
         }
 
+
         // Method which does operation on balance
-        private void GetCash(int cash, User[] users, int userId)
+        private void GetCash(int cash, User user)
         {
             if (cash < 0)
             {
                 Console.WriteLine("Invalid cash, Please try again");
             }
-            else if (cash > users[userId].Balance)
+            else if (cash > user.Creditcard.Balance)
             {
                 Console.WriteLine("No Enough money on your Balance");
             }
             else
             {
-                users[userId].Balance -= cash;
-                //Console.WriteLine("Loading ...");
-                //Console.WriteLine($"Process finished.");
-                Console.WriteLine($"On Balance Left: {users[userId].Balance} AZN\n\n");
+                user.Creditcard.Balance -= cash;
+                Console.WriteLine("Loading ...");
+                Console.WriteLine($"Process finished.");
+                Console.WriteLine($"On Balance Left: {user.Creditcard.Balance} AZN\n\n");
             }
-            OperationDecider(users, userId);
+            OperationDecider(user);
         }
     }
+
 }
